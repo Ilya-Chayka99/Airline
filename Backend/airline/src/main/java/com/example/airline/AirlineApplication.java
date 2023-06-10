a569df724ac833f4cdb0f6d56eb71c5a57ac696b
Ilya-Chayka99 <Ilya-Chayka99@yandex.ru>
2023-06-10 19:22:33 +0400

    spring boot admin

--- Backend/airline/src/main/java/com/example/airline/AirlineApplication.java
+++ Backend/airline/src/main/java/com/example/airline/AirlineApplication.java
@@ -1,10 +1,12 @@
 package com.example.airline;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAdminServer
 public class AirlineApplication {
 
     public static void main(String[] args) {
